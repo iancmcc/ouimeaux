606ab5fe8d0a1ba8ad62e44838e9f42fb3ff5028
Ian McCracken <ian.mccracken@gmail.com>
2013-02-01 11:05:30 -0600

    Make some methods private

--- ouimeaux/discover.py
+++ ouimeaux/discover.py
@@ -17,28 +17,29 @@ class StopBroadcasting(Exception): pass
 
 class Manager(object):
     def __init__(self, switch_callback=NOOP, motion_callback=NOOP):
-        self.upnp = UPnP(self.found_device)
+        self.upnp = UPnP(self._found_device)
         self._switch_callback = switch_callback
         self._motion_callback = motion_callback
         self.switches = {}
         self.motions = {}
 
-    def discover(self, timeout=10):
+    def discover(self, seconds=10):
         log.info("Beginning discovery of devices")
         self.upnp.server.set_spawn(2)
         self.upnp.server.start()
-        with gevent.Timeout(timeout, StopBroadcasting) as timeout:
+        with gevent.Timeout(seconds, StopBroadcasting) as timeout:
             try:
-                while True:
-                    try:
+                try:
+                    while True:
                         self.upnp.broadcast()
                         gevent.sleep(1)
-                    except Exception as e:
-                        raise StopBroadcasting(e)
+                except Exception as e:
+                    raise StopBroadcasting(e)
             except StopBroadcasting:
+                self.upnp.server.stop()
                 return
 
-    def found_device(self, address, headers):
+    def _found_device(self, address, headers):
         usn = headers['usn']
         if usn.startswith('uuid:Socket'):
             switch = Switch(headers['location'])
