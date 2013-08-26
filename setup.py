ce3f8ad5adac617427155fc3a8b9bc469769c71f
Ian McCracken <ian.mccracken@gmail.com>
2013-08-25 22:50:32 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.4'
+version = '0.4.1'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     description = f.read()
