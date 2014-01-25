1b815768225830ae5d98d71bd06b88f90c5d9147
Ian McCracken <ian.mccracken@gmail.com>
2014-01-25 11:15:12 -0600

    Add insight class

---
+++ ouimeaux/insight.py
@@ -0,0 +1,34 @@
+from ouimeaux.switch import Switch
+
+class Insight(Switch):
+
+    def __repr__(self):
+        return '<WeMo Insight "{name}">'.format(name=self.name)
+
+    @property
+    def today_kwh(self):
+        return self.insight.GetTodayKWH()
+
+    @property
+    def current_power(self):
+        """
+        Returns the current power usage in mW.
+        """
+        return self.insight.GetPower()
+
+    @property
+    def today_on_time(self):
+        return self.insight.GetTodayONTime()
+
+    @property
+    def on_for(self):
+        return self.insight.GetONFor()
+
+    @property
+    def in_standby_since(self):
+        return self.insight.GetInSBYSince()
+
+    @property
+    def today_standby_time(self):
+        return self.insight.GetTodaySBYTime()
+
