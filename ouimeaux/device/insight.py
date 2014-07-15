9d2a55c5a4c0bc8f99232e0435dc34af20ca030d
Ian McCracken <ian.mccracken@gmail.com>
2014-07-14 21:47:53 -0500

    Saving state

--- ouimeaux/device/insight.py
+++ ouimeaux/device/insight.py
@@ -6,6 +6,48 @@ class Insight(Switch):
         return '<WeMo Insight "{name}">'.format(name=self.name)
 
     @property
+    def insight_params(self):
+        params = self.insight.GetInsightParams().get('InsightParams')
+['1',
+ '1401624771',
+ '493',
+ '513',
+ '161285',
+ '1209600',
+
+ '20',
+ '19580',
+ '159187',
+ '49942514.000000',
+ '8000']
+        (
+            state,
+            lastchange,
+            onfor,
+            ontoday,
+            ontotal,
+            timeperiod,
+            x,
+            currentmw,
+            z,
+            j,
+            powerthreshold
+
+
+        )
+        state, lastchange, onfor, ontoday, ontotal, twoweeks, todaymw, totalmw, u3 = params.split('|')
+        return {
+                'state': state,
+                'lastchange': datetime.fromtimestamp(int(lastchange)),
+                'onfor': int(laston),
+                'ontoday': int(ontoday),
+                'ontotal': int(total),
+                'todaymw': todaymw,
+                'totalmw': totalmw,
+                'currentpower': u2
+                }
+
+    @property
     def today_kwh(self):
         return self.insight.GetTodayKWH()
 
