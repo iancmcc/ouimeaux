c8ef4dd1c69a90ff992431c6a267c12c922624f6
Eric Secules <esecules@gmail.com>
2016-04-09 01:01:33 -0700

    watch example now ignores updates where state doesnt change. Perhaps I should bake this into signals.py next...

--- ouimeaux/examples/watch.py
+++ ouimeaux/examples/watch.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 import argparse
 import sys
-
+import traceback
 from ouimeaux.environment import Environment
 from ouimeaux.utils import matcher
 from ouimeaux.signals import receiver, statechange, devicefound
 
+class Watcher(object):
+    def __init__(self):
+        self.state = None
 
-def mainloop(name):
-    matches = matcher(name)
+    def mainloop(self, name):
+        matches = matcher(name)
 
-    @receiver(devicefound)
-    def found(sender, **kwargs):
-        if matches(sender.name):
-            print "Found device:", sender.name
-
-    @receiver(statechange)
-    def motion(sender, **kwargs):
-        if matches(sender.name):
-            print "{} state is {state}".format(
-                sender.name, state="on" if kwargs.get('state') else "off")
-
-    env = Environment()
-    try:
-        env.start()
-        env.discover(10)
-        env.wait()
-    except (KeyboardInterrupt, SystemExit):
-        print "Goodbye!"
-        sys.exit(0)
+        @receiver(devicefound)
+        def found(sender, **kwargs):
+            if matches(sender.name):
+                print "Found device:", sender.name
+    
+        @receiver(statechange)
+        def motion(sender, **kwargs):
+            if matches(sender.name) and self.state != kwargs.get('state'):
+                self.state = kwargs.get('state')
+                print "{} state is {state}".format(
+                    sender.name, state="on" if kwargs.get('state') else "off")
+        env = Environment()
+        try:
+            env.start()
+            env.discover(10)
+            env.wait()
+        except (KeyboardInterrupt, SystemExit):
+            print "Goodbye!"
+            sys.exit(0)
 
 
 if __name__ == "__main__":
@@ -37,4 +40,5 @@ if __name__ == "__main__":
                         help="Name (fuzzy matchable)"
                              " of the Motion to detect")
     args = parser.parse_args()
-    mainloop(args.name)
+    w = Watcher()
+    w.mainloop(args.name)
